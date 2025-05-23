d6e54ea036307e86c242937dabfb4a26ccbf3c96
Harman Bhogal <bhogalharman7@gmail.com>
2025-05-22 18:07:17 -0700

    added push and pop of undostack

--- src/redo.cpp
+++ src/redo.cpp
@@ -2,4 +2,21 @@
 
 //history implmenetatonis
 
-void History::action(const std::vector<std::string>& state)
+void History::pushState(const State& state){
+    if(undoStack.size()>= MAX_HISTORY){
+        undoStack.erase(undoStack.begin());
+    }
+    undoStack.push_back(state);
+    redoStack.clear();
+}
+
+bool History::undo(State& state){
+    if(undoStack.empty()){
+        return false;
+    }
+    redoStack.push_back(state);
+    state = undoStack.back();
+    undoStack.pop_back();
+    return true;
+
+}
