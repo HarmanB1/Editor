5fd3ec936ba3e55ae42e48d7e52d18df71f06c71
Harman Bhogal <bhogalharman7@gmail.com>
2025-05-22 17:42:59 -0700

    added more defentions nad changed some defention of redo.h

--- src/redo.cpp
+++ src/redo.cpp
@@ -1,1 +1,5 @@
 #include "redo.h"
+
+//history implmenetatonis
+
+void History::action(const std::vector<std::string>& state)
--- src/redo.h
+++ src/redo.h
@@ -1,7 +1,41 @@
 #pragma once
 #include <vector>
 #include <string>
+struct State{
+    std::vector<std::string> content;
+    size_t cursorX;
+    size_t cursorY;
+    std::string filepath;
+    std::string directory;
+
+    //initializer
+    State(
+        std::vector<std::string> c,
+        size_t x,
+        size_t y,
+        std::string f,
+        std::string d)
+    
+        : content(c), cursorX(x), cursorY(y), filepath(f), directory(d){}
+
+    
+};
+
 class History{
+    public:
+        
+        void pushState(const State& state);
+        bool undo(State& state);
+        bool redo(State& state);
+
+    private:
+        std::vector<State> undoStack;
+        std::vector<State> redoStack;
+        const int MAX_HISTORY = 100;
+
+
+
+
 
 
 };
