6fb928740e4ef518f7e8863bf5d3ac9c23812982
Harman Bhogal <bhogalharman7@gmail.com>
2025-05-25 22:41:29 -0700

    documentation for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "editor.h"
 
+/**
+ * @file main.cpp
+ * @brief Entry point for the text editor application.
+ * 
+ * Creates an Editor instance and starts the main program loop.
+ */
+
 int main() {
     Editor editor;
     editor.run();
